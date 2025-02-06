dcd0b8f544e3bbbd09c0ea14fe80e2719c222750
PrymushAnton <anton.primush123663@gmail.com>
2025-02-06 21:17:06 +0200

    Deleted second modal for regPage. Now there is only one modal for both pages

--- src/shared/AuthRegModal/AuthRegModal.tsx
+++ src/shared/AuthRegModal/AuthRegModal.tsx
@@ -1,76 +1,93 @@
 import { useState } from "react";
 import "./AuthRegModal.css"
 import Modal from 'react-bootstrap/Modal';
-import { ModalBody, ModalFooter, ModalHeader } from "react-bootstrap";
 
 export function AuthModal(){
-    const [showLog, setShowLog] = useState(false);
-    const [showReg, setShowReg] = useState(false);
 
-    const handleCloseLog = () => setShowLog(false);
-    const handleCloseReg = () => setShowReg(false);
+    const [isLogShown, setIsLogShown] = useState<boolean>(false)
+    const [isRegShown, setIsRegShown] = useState<boolean>(false)
+    const [isModalOpen, setIsModalOpen] = useState<boolean>(false)
 
-    const handleShowLog = () => {
-        setShowLog(true);
-        setShowReg(false);
-    };
+    function openModal() {
+        setIsModalOpen(true)
+        setIsLogShown(true)
+        setIsRegShown(false)
+    }
 
-    const handleShowReg = () => {
-        setShowReg(true);
-        setShowLog(false);
-    };
+    function closeModal() {
+        setIsModalOpen(false)
+        setIsLogShown(false)
+        setIsRegShown(false)
+    }
+
+    function openLogIn() {
+        setIsLogShown(true)
+        setIsRegShown(false)
+    }
+
+    function openRegIn() {
+        setIsLogShown(false)
+        setIsRegShown(true)
+
+        console.log(isLogShown ? isLogShown : isRegShown ?? isRegShown)
+    }
+
+
+
     return(
         <div>
-            <button id="loginButtonHeader" onClick={handleShowLog}>
+            <button id="loginButtonHeader" onClick={() => {openModal()}}>
                 Login
                 <svg width="10" height="14" viewBox="0 0 10 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10 11.1941C10 13.9784 7.76142 13.4627 5 13.4627C2.23858 13.4627 0 13.9784 0 11.1941C0 8.40974 2.23858 6.15259 5 6.15259C7.76142 6.15259 10 8.40974 10 11.1941Z" fill="white"/><ellipse cx="4.99989" cy="3.04348" rx="2.36842" ry="2.54348" fill="white"/></svg>
             </button>
-            <Modal show={showLog} onHide={handleCloseLog} id="AuthModal">
+            <Modal show={isModalOpen} onHide={() => {closeModal()}} id="AuthModal">
                 <Modal.Header id="AuthRegTop">
-                    <h2>Sign In</h2>
+                    {
+                        isLogShown
+                        ? <h2>Log In</h2>
+                        : isRegShown && <h2>Sign Up</h2>
+                    }
                 </Modal.Header>
-                <form>
-                    <Modal.Body id="AuthRegMiddle">
+                <Modal.Body id="AuthRegMiddle">
+                    <form>
+                        
                         <div className="inputDiv">
-                            <h4 className="inputTitle">Email</h4>
+                            <h4 className="inputTitle">Username</h4>
                             <input type="text" className="inputText"/>
                         </div>
-                        <div className="inputDiv">
-                            <h4 className="inputTitle">Password</h4>
-                            <input type="password" className="inputText"/>
-                        </div>
-                    </Modal.Body>
-                    <Modal.Footer id="AuthRegBottom">
-                        <input type="submit" value="Sign In" id="submitButt"/>
-                        <button type="button" id="SignButt"  onClick={handleShowReg} >Sign Up</button>
-                    </Modal.Footer>
-                </form>
-            </Modal>
 
-            <Modal show={showReg} onHide={handleCloseReg} id="RegModal">
-                <ModalHeader id="AuthRegTop">
-                    <h2>Sign Up</h2>
-                </ModalHeader>
-                <form>
-                    <ModalBody id="AuthRegMiddle">
-                        <div className="inputDiv">
-                            <h4  className="inputTitle">Name</h4>
-                            <input type="text" className="inputText"/>
-                        </div>
-                        <div className="inputDiv">
-                            <h4 className="inputTitle">Email</h4>
-                            <input type="text" className="inputText"/>
-                        </div>
+                        {
+                            isRegShown 
+                            && <div className="inputDiv">
+                                <h4 className="inputTitle">Email</h4>
+                                <input type="text" className="inputText"/>
+                            </div>
+                        }
+                        
                         <div className="inputDiv">
                             <h4 className="inputTitle">Password</h4>
                             <input type="password" className="inputText"/>
                         </div>
-                    </ModalBody>
-                    <ModalFooter id="AuthRegBottom">
-                        <input type="submit" value="Sign Up" id="submitButt"/>
-                        <button type="button" id="SignButt" onClick={handleShowLog}>Sign In</button>
-                    </ModalFooter>
-                </form>
+                    </form>
+                </Modal.Body>
+                
+                <Modal.Footer id="AuthRegBottom">
+
+                    {
+                        isLogShown
+                        ? <div>
+                            <input type="submit" value="Log In" id="submitButt"/>
+                            <button type="button" id="SignButt" onClick={() => {openRegIn()}} >Sign Up</button>
+                        </div>
+                        : isRegShown && <div>
+                            <input type="submit" value="Sign Up" id="submitButt"/>
+                            <button type="button" id="SignButt" onClick={() => {openLogIn()}} >Log In</button>
+                        </div>
+                   
+
+                    }
+                </Modal.Footer>
+                
             </Modal>
         </div>
     )
