b5879c0be710211530f8d946c60581d73c708aa7
RinatUA <feawsq@gmail.com>
2025-02-14 09:45:02 +0200

    some merges

--- src/shared/AuthRegModal/AuthRegModal.tsx
+++ src/shared/AuthRegModal/AuthRegModal.tsx
@@ -1,69 +1,40 @@
 import { useState } from "react";
 import "./AuthRegModal.css"
 import Modal from 'react-bootstrap/Modal';
+import { ModalBody, ModalFooter, ModalHeader } from "react-bootstrap";
 
 export function AuthModal(){
+    const [showLog, setShowLog] = useState(false);
+    const [showReg, setShowReg] = useState(false);
 
-    const [isLogShown, setIsLogShown] = useState<boolean>(false)
-    const [isRegShown, setIsRegShown] = useState<boolean>(false)
-    const [isModalOpen, setIsModalOpen] = useState<boolean>(false)
+    const handleCloseLog = () => setShowLog(false);
+    const handleCloseReg = () => setShowReg(false);
 
-    function openModal() {
-        setIsModalOpen(true)
-        setIsLogShown(true)
-        setIsRegShown(false)
-    }
+    const handleShowLog = () => {
+        setShowLog(true);
+        setShowReg(false);
+    };
 
-    function closeModal() {
-        setIsModalOpen(false)
-        setIsLogShown(false)
-        setIsRegShown(false)
-    }
-
-    function openLogIn() {
-        setIsLogShown(true)
-        setIsRegShown(false)
-    }
-
-    function openRegIn() {
-        setIsLogShown(false)
-        setIsRegShown(true)
-
-        console.log(isLogShown ? isLogShown : isRegShown ?? isRegShown)
-    }
-
-
-
+    const handleShowReg = () => {
+        setShowReg(true);
+        setShowLog(false);
+    };
     return(
         <div>
-            <button id="loginButtonHeader" onClick={() => {openModal()}}>
+            <button id="loginButtonHeader" onClick={handleShowLog}>
                 Login
                 <svg width="10" height="14" viewBox="0 0 10 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10 11.1941C10 13.9784 7.76142 13.4627 5 13.4627C2.23858 13.4627 0 13.9784 0 11.1941C0 8.40974 2.23858 6.15259 5 6.15259C7.76142 6.15259 10 8.40974 10 11.1941Z" fill="white"/><ellipse cx="4.99989" cy="3.04348" rx="2.36842" ry="2.54348" fill="white"/></svg>
             </button>
             <Modal show={showLog} onHide={handleCloseLog} id="AuthModal" animation={false}>
                 <Modal.Header id="AuthRegTop">
-                    {
-                        isLogShown
-                        ? <h2>Log In</h2>
-                        : isRegShown && <h2>Sign Up</h2>
-                    }
+                    <h2>Sign In</h2>
                 </Modal.Header>
-                <Modal.Body id="AuthRegMiddle">
-                    <form>
-                        
+                <form>
+                    <Modal.Body id="AuthRegMiddle">
                         <div className="inputDiv">
-                            <h4 className="inputTitle">Username</h4>
+                            <h4 className="inputTitle">Email</h4>
                             <input type="text" className="inputText"/>
                         </div>
-
-                        {
-                            isRegShown 
-                            && <div className="inputDiv">
-                                <h4 className="inputTitle">Email</h4>
-                                <input type="text" className="inputText"/>
-                            </div>
-                        }
-                        
                         <div className="inputDiv">
                             <div id="inputDivTop">
                                 <h4 className="inputTitle">Password</h4>
@@ -90,9 +61,9 @@ export function AuthModal(){
                             <h4  className="inputTitle">Name</h4>
                             <input type="text" className="inputText"/>
                         </div>
-                        : isRegShown && <div>
-                            <input type="submit" value="Sign Up" id="submitButt"/>
-                            <button type="button" id="SignButt" onClick={() => {openLogIn()}} >Log In</button>
+                        <div className="inputDiv">
+                            <h4 className="inputTitle">Email</h4>
+                            <input type="text" className="inputText"/>
                         </div>
                         <div className="inputDiv">
                             <h4 className="inputTitle">Password</h4>
